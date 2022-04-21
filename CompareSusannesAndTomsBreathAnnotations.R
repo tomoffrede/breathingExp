8b8622e0792d98e77a38fff87ec2994763c58665
Tom Offrede <tom.offrede@gmail.com>
2022-04-21 14:18:34 +0200

    added difference calculation

--- CompareSusannesAndTomsBreathAnnotations.R
+++ CompareSusannesAndTomsBreathAnnotations.R
@@ -1,7 +1,6 @@
 ### calculate difference in time points between Tom's and Susanne's annotations
 
 library(rPraat)
-library(tidyverse)
 
 foldert <- "C:/Users/tomof/Documents/1HU/ExperimentBreathing/Data/DataForAnalysis/Confederate/BreathingWithEntireCycle-Free/"
 folders <- "C:/Users/tomof/Documents/1HU/ExperimentBreathing/Data/DataForAnalysis/Confederate/BreathingWithEntireCycle-Free/Susanne/"
@@ -23,21 +22,31 @@ for(i in 1:nrow(file)){
   ds <- data.frame(matrix(ncol=3, nrow=0))
   names(ds) <- c("file", "pointS", "timeS")
   for(p in 1:tg.getNumberOfPoints(s, 1)){
-    ds[nrow(ds)+1,] <- c(filename, paste0("peak", p), tg.getPointTime(s, 1, p))
+    ds[nrow(ds)+1,] <- c(filename, paste0("peak", p), as.numeric(tg.getPointTime(s, 1, p)))
   }
   for(v in 1:tg.getNumberOfPoints(s, 2)){
-    ds[nrow(ds)+1,] <- c(filename, paste0("valley", v), tg.getPointTime(s, 2, v))
+    ds[nrow(ds)+1,] <- c(filename, paste0("valley", v), as.numeric(tg.getPointTime(s, 2, v)))
   }
   
   dt <- data.frame(matrix(ncol=2, nrow=0))
   names(dt) <- c("pointT", "timeT")
   for(p in 1:tg.getNumberOfPoints(t, 1)){
-    dt[nrow(dt)+1,] <- c(paste0("peak", p), tg.getPointTime(t, 1, p))
+    dt[nrow(dt)+1,] <- c(paste0("peak", p), as.numeric(tg.getPointTime(t, 1, p)))
   }
   for(v in 1:tg.getNumberOfPoints(t, 2)){
-    dt[nrow(dt)+1,] <- c(paste0("valley", v), tg.getPointTime(t, 2, v))
+    dt[nrow(dt)+1,] <- c(paste0("valley", v), as.numeric(tg.getPointTime(t, 2, v)))
   }
   
   d <- data.frame(cbind(ds, dt))
   diff <- rbind(diff, d)
 }
+
+diff$timeS <- as.numeric(diff$timeS)
+diff$timeT <- as.numeric(diff$timeT)
+
+diff$timeDiff <- diff$timeS - diff$timeT
+
+mean(diff$timeDiff)
+sd(diff$timeDiff)
+
+hist(diff$timeDiff)
